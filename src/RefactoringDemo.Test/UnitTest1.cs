d5954e7f9706b1e4bb353b4b370b9890db86f9b6
Maicon Heck <13810070+maiconheck@users.noreply.github.com>
2020-03-09 09:34:20 +0000

    :hankey: a very poor test example

---
+++ src/RefactoringDemo.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace RefactoringDemo.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // I will test when I have enough time.
+            Assert.True(true);
+        }
+    }
+}
